066db0cfe402f0ed341159da9ae61df2ad1fad84
gsalaun1 <salaunga@yahoo.fr>
2019-05-22 17:46:00 +0200

    Utilisation de LocalDate dans les stepdefs

--- src/test/kotlin/bank/features/StepDefs.kt
+++ src/test/kotlin/bank/features/StepDefs.kt
@@ -2,6 +2,7 @@ package bank.features
 
 import cucumber.api.PendingException
 import cucumber.api.java8.En
+import java.time.LocalDate
 
 /**
  *
@@ -9,13 +10,13 @@ import cucumber.api.java8.En
  */
 class StepDefs : En {
     init {
-        Given("^a client makes a deposit of (\\d+) on (\\d+)-(\\d+)-(\\d+)$"
-        ) { arg0: Int, arg1: Int, arg2: Int, arg3: Int -> throw PendingException() }
-        And("^a deposit of (\\d+) on (\\d+)-(\\d+)-(\\d+)$"
-        ) { arg0: Int, arg1: Int, arg2: Int, arg3: Int -> throw PendingException()}
-        And("^a withdrawal of (\\d+) on (\\d+)-(\\d+)-(\\d+)$"
-        ) { arg0: Int, arg1: Int, arg2: Int, arg3: Int -> throw PendingException()}
-        When("^she prints her bank statement$") { throw PendingException()}
-        Then("^she would see$") {arg0 : String -> throw PendingException()}
+        Given("a client makes a deposit of {int} on {localdate}"
+        ) { arg0: Int, arg1: LocalDate -> throw PendingException() }
+        And("a deposit of {int} on {localdate}"
+        ) { arg0: Int, arg1: LocalDate -> throw PendingException() }
+        And("a withdrawal of {int} on {localdate}"
+        ) { arg0: Int, arg1: LocalDate -> throw PendingException() }
+        When("she prints her bank statement") { throw PendingException() }
+        Then("she would see") { arg0: String -> throw PendingException() }
     }
 }
